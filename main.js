6893b6700f7bb70491c55116312c4136881f8b92
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-03-10 10:33:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var lessonId = html5Iframe.src.split("/")[html5Iframe.src.split("/").length - 1];
+var lessonType = lessonId.split("_")[lessonId.split("_").length - 1];
+
 var slideCount;
 
-fetch(`https://login.i-ready.com/student/v2/web/appstate/${lessonId.split("_").join(".").substring(0, lessonId.split("_").join(".").length - 3)}.phx.10_71da0b35-cb71-4540-9e75-73203c6686dc_M_math?bucket=short_term_unsecured&datatype=json`, {
+fetch(`https://login.i-ready.com/student/v2/web/appstate/${lessonId.split("_").join(".").substring(0, lessonId.split("_").join(".").length - 3)}.phx.${lessonType}_71da0b35-cb71-4540-9e75-73203c6686dc_M_math?bucket=short_term_unsecured&datatype=json`, {
 		"headers": {
 			"accept": "application/json, text/plain, */*",
 			"accept-language": "en-US,en;q=0.9",

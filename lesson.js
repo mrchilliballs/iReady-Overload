a13ea0cf13e67a2bf64409828791ffb71306077a
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-03-13 14:12:23 -0500

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -1,12 +1,15 @@
+// gets lesson data
 var lessonId = html5Iframe.src.split("/")[html5Iframe.src.split("/").length - 1];
 var lessonType = lessonId.split("_")[lessonId.split("_").length - 1];
 var csid = html5Iframe.src.split("?csid=")[1].split("&type")[0];
 var subject = csid.includes("math") ? "math" : "reading";
 
+// empty global variables for later use
 var slidesCompletedArr = [];
 var scoreArr = [];
 var slideCount;
 
+// gets lesson strucure
 html5Iframe.contentWindow.fetch(`https://cdn.i-ready.com/instruction/phoenix-content/${subject}/${lessonId}/lessonStructure.json`, {
 		"headers": {
 			"accept": "application/json, text/plain, */*",
@@ -30,11 +33,13 @@ console.log();
 		slidesCompletedArr = [];
 		scoreArr = [];
 
+        // creates score and completion arrays with the length neccessary
 		for (var i = 0; i < slideCount; i++) {
 			slidesCompletedArr.push(true);
 			scoreArr.push(1);
 		}
 
+        // sets cookies for external.js to use
 		document.cookie = `lessonId=${lessonId};`
 		document.cookie = `lessonType=${lessonType};`
 		document.cookie = `subject=${subject};`

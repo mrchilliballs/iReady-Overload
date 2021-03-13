ee5356ff6c5bd152abaf75393dcd33544426b868
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-03-13 14:13:28 -0500

    Update resetLesson.js

--- resetLesson.js
+++ resetLesson.js
@@ -1,14 +1,17 @@
 // this will reset a lesson or quiz to its original state
 
+// gets lesson data
 var lessonId = html5Iframe.src.split("/")[html5Iframe.src.split("/").length - 1];
 var lessonType = lessonId.split("_")[lessonId.split("_").length - 1];
 var csid = html5Iframe.src.split("?csid=")[1].split("&type")[0];
 var subject = csid.includes("math") ? "math" : "reading";
 
+// empty global variables for later use
 var slidesCompletedArr = [];
 var scoreArr = [];
 var slideCount;
 
+// gets lesson strucure
 html5Iframe.contentWindow.fetch(`https://cdn.i-ready.com/instruction/phoenix-content/${subject}/${lessonId}/lessonStructure.json`, {
 		"headers": {
 			"accept": "application/json, text/plain, */*",
@@ -31,11 +34,13 @@ html5Iframe.contentWindow.fetch(`https://cdn.i-ready.com/instruction/phoenix-con
 		slidesCompletedArr = [];
 		scoreArr = [];
 
+		// creates score and completion arrays with the length neccessary
 		for (var i = 0; i < slideCount; i++) {
 			slidesCompletedArr.push(false);
 			scoreArr.push(1);
 		}
 
+		// sets cookies for external.js to use
 		document.cookie = `lessonId=${lessonId};`
 		document.cookie = `lessonType=${lessonType};`
 		document.cookie = `subject=${subject};`

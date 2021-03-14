c4f7f9c85ebd30aee436f9b0ba3d51df2f9b9c13
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-03-13 20:17:03 -0500

    Update resetLesson.js

--- resetLesson.js
+++ resetLesson.js
@@ -1,8 +1,8 @@
 // this will reset a lesson or quiz to its original state
 
 // gets lesson data
+var lessonId = html5Iframe.src.split("/")[html5Iframe.src.split("/").length - 1];
 var csid = html5Iframe.src.split("?csid=")[1].split("&type")[0];
-var lessonId = (csid.split(".phx")[0] + csid.split(".phx")[1].split("_")[0]).split(".").join("_");
 var lessonType = lessonId.split("_")[lessonId.split("_").length - 1];
 var subject = csid.includes("math") ? "math" : "reading";
 

f537a51fd030322e0881a4c41f8bf265e8be76af
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-04-06 15:37:23 -0400

    Update lesson.js

--- minutesHack/lesson.js
+++ minutesHack/lesson.js
@@ -3,5 +3,5 @@ var csid = html5Iframe.src.split("?csid=")[1].split("&type")[0];
 var minutes = 45;
 
 // sets cookies in case something breaks
-document.cookie = `csid=${csid};`
+document.cookie = `csid=${csid}; expires=Thu, 18 Dec 2999 12:00:00 UTC"`
 document.cookie = `minutes=${minutes};`

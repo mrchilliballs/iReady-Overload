dfbb271a0142456bc79781c4fd3d1cb65c713fea
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-04-06 14:22:53 -0400

    Update lesson.js

--- minutesHack/lesson.js
+++ minutesHack/lesson.js
@@ -1,3 +1,7 @@
 // gets lesson data
 var csid = html5Iframe.src.split("?csid=")[1].split("&type")[0];
-var minutes = 45; // change the "45" to the number of minutes you want.
+var minutes = 45;
+
+// sets cookies in case something breaks
+document.cookie = `csid=${csid};`
+document.cookie = `minutes=${minutes};`

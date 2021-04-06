1a837c7d9ef01024386edf3a6dda5c7a4811861b
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-04-06 15:52:33 -0400

    Update lesson.js

--- minutesHack/lesson.js
+++ minutesHack/lesson.js
@@ -4,4 +4,4 @@ var minutes = 45;
 
 // sets cookies in case something breaks
 document.cookie = `csid=${csid}; expires=Thu, 18 Dec 2999 12:00:00 UTC"`
-document.cookie = `minutes=${minutes};`
+document.cookie = `minutes=${minutes}; expires=Thu, 18 Dec 2999 12:00:00 UTC"`

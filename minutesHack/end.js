49867f71026d12ae13f960227129cd0f77c370c8
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-04-06 21:49:25 -0400

    Update end.js

--- minutesHack/end.js
+++ minutesHack/end.js
@@ -1,4 +1,4 @@
-// sends fetch request to stop timer
+// sends fetch request to stop timer and update time
 fetch(`https://login.i-ready.com/student/v1/web/lesson_component/${csid}?action=pause`, {
 	"headers": {
 		"accept": "application/json, text/plain, */*",

11895d9b8c69c89c0c479a9156edcce06c55a583
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2021-04-06 21:49:10 -0400

    Update external.js

--- minutesHack/external.js
+++ minutesHack/external.js
@@ -1,4 +1,4 @@
-// sends fetch request to stop timer
+// sends fetch request to start timer
 fetch(`https://login.i-ready.com/student/v1/web/lesson_component/${csid}?action=resume`, {
 	"headers": {
 		"accept": "application/json, text/plain, */*",
